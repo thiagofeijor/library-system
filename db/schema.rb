06a51369187356673952697b46a742a3c28c40fe
Thiago Feijó <thiago.feijo13@gmail.com>
2018-10-22 20:13:48 -0300

    Mudança para PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20181021184430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
